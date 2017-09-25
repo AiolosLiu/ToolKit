735dbf07a9ee32dfa6940426dc36cc45b14df1b4
AiolosLiu <windbeyondsky@gmail.com>
2017-09-24 20:46:09 -0700

    Luke: add Union Find.

--- Java/Algorithms/UnionFind.java
+++ Java/Algorithms/UnionFind.java
@@ -7,8 +7,9 @@ public class UnionFind{
 		// Initialize component id array.
 		count = N;
 		id = new int[N];
-		for (int i = 0; i < N; i++)
-			id[i] = i;
+		for (int i = 0; i < N; i++){
+			id[i] = i;			
+		}
 	}
 	
 	public int count(){ 
@@ -23,13 +24,18 @@ public class UnionFind{
 		return id[p]; 
 	}
 	
-	public void union(int p, int q)
-	{ 
+	public void union(int p, int q){ 
 		int pID = find(p);
 		int qID = find(q);
-		if (pID == qID) return;
-		for (int i = 0; i < id.length; i++)
+
+		if (pID == qID){
+			return;
+		}
+		
+		for (int i = 0; i < id.length; i++){
 			if (id[i] == pID) id[i] = qID;
+		}
+			
 		-- count;
 	}
 }
